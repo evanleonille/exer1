f10b760c5b42ec0b8a5c30589d590258a229c9b1
evanleonille <133546962+evanleonille@users.noreply.github.com>
2023-09-08 10:11:31 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -2,14 +2,19 @@ function displayProduct() {
   let firstNum = Number(document.getElementById('firstNum').innerHTML)
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
-  let total = firstNum + secondNum;
+  let total = firstNum * secondNum;
   document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
 
 
 function displayDiff() {
-  
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+
+  let total = firstNum - secondNum;
+  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+}
 }
   
 
